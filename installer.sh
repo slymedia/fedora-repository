1447f052081901411e7bc7db5371592b7f0e0dc6
Sly Media Networks <support@slynet.com>
2015-07-15 22:49:00 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -45,6 +45,7 @@ export JAVA_HOME=${INSTALL_PREFIX}/java
 # Note: be careful using regular expression characters in passwords. Be sure to escape them
 #
 export INSTALL_CSF="Yes"                                # Install the CSF firewall
+export ALLOW_ONLY_US="Yes"                              # Only allow US access to the server
 export INSTALL_MYSQL="Yes"                              # Yes or No for installing Mysqld locally on machine
 export ROOT_MYSQL_PASS='mysqlroot'                      # If Yes to installing MySQL set the root user password
 export DB_SERVER=localhost
@@ -129,6 +130,9 @@ if [ $INSTALL_CSF == "Yes" ]; then
     sed -i 's|SYSLOG_CHECK = "0"|SYSLOG_CHECK = "300"|g' /etc/csf/csf.conf
     sed -i 's|PT_ALL_USERS = "0"|PT_ALL_USERS = "1"|g' /etc/csf/csf.conf
     sed -i 's|PT_USERMEM = "200"|PT_USERMEM = "350"|g' /etc/csf/csf.conf
+    if [ $ALLOW_ONLY_US == "Yes" ]; then
+        sed -i 's|CC_ALLOW_FILTER = ""|CC_ALLOW_FILTER = "US"|g' /etc/csf/csf.conf
+    fi
     # add ports
     sed -i 's|TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"|TCP_IN = "20,21,22,25,53,80,443,465,587,993,995,8080,9418"|g' /etc/csf/csf.conf
     sed -i 's|TCP_OUT = "20,21,22,25,53,80,110,113,443,587,993,995"|TCP_OUT = "20,21,22,25,53,80,113,443,587,993,995,8080,9418"|g' /etc/csf/csf.conf
