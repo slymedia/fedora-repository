dd4d3c461f3c0069204d7123ec2a5306d9800758
Sly Media Networks <support@slynet.com>
2015-07-28 08:25:47 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -46,7 +46,7 @@ export JAVA_HOME=${INSTALL_PREFIX}/java
 #
 export INSTALL_CSF="No"                                # Install the CSF firewall
 export ALLOW_ONLY_US="Yes"                              # Only allow US access to the server
-export INSTALL_MYSQL="Yes"                              # Yes or No for installing Mysqld locally on machine
+export INSTALL_MYSQL="No"                              # Yes or No for installing Mysqld locally on machine
 export ROOT_MYSQL_PASS='mysqlroot'                      # If Yes to installing MySQL set the root user password
 export DB_SERVER=localhost
 export FEDORA_DB_NAME="fedora3"        		            # Name of fedora MySQL database -> fedora3 is recommended
@@ -89,7 +89,7 @@ cd $REPOSITORY_HOME/sbin
 script -ac ./software_dependencies.install ${INSTALL_LOG}
 #script -ac ./apache.install ${INSTALL_LOG}
 if [ $INSTALL_MYSQL == "Yes" ]; then
-    #script -ac ./mysql.install ${INSTALL_LOG}
+    script -ac ./mysql.install ${INSTALL_LOG}
 fi
 #script -ac ./fedora.install ${INSTALL_LOG}
 #script -ac ./gsearch.install ${INSTALL_LOG}
